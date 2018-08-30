481384bf8af6f1400bf3ad7111324d8c5216ce33
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-08-30 10:31:50 -0500

    found out in their test they reassign DIGIT_VALUES in the background which is why it was breaking when i made it const, and why when i was trying to use a null key for zero it was never being used. because in the background it was being taken away without me knowing

--- problems/romanNumeralTranslator.js
+++ problems/romanNumeralTranslator.js
@@ -1,8 +1,8 @@
  /*! toyproblems v0.0.0 - MIT license */
 'use strict';
+'use strict';
 
-const DIGIT_VALUES = {
-  null: 0,
+var DIGIT_VALUES = {
   I: 1,
   V: 5,
   X: 10,
@@ -29,46 +29,18 @@ const translateRomanNumeral = function(romanNumeral){
     return null;
   }
   at = 0;
+
   
   let value = 0;
 
   let currentContext = next(romanNumeral);
   while (currentContext[1]) {
-    if ( DIGIT_VALUES[currentContext[0]] >= DIGIT_VALUES[currentContext[1]]) {
+    
+    if ( DIGIT_VALUES[currentContext[0]] >= DIGIT_VALUES[currentContext[1]] || currentContext[0] === null) {
       value += DIGIT_VALUES[currentContext[1]];
     } else if (DIGIT_VALUES[currentContext[0]] < DIGIT_VALUES[currentContext[1]]) {
       value += DIGIT_VALUES[currentContext[1]] - (DIGIT_VALUES[currentContext[0]] * 2);
     }
-    // if (currentContext[1] === 'I') {
-    //   value += DIGIT_VALUES[currentContext[1]];
-    // } else if (currentContext[1] === 'V' && DIGIT_VALUES[currentContext[0]] >= DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]];
-    // } else if (currentContext[1] === 'V' && DIGIT_VALUES[currentContext[0]] < DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]] - (DIGIT_VALUES[currentContext[0]] * 2);
-    // } else if (currentContext[1] === 'X' && DIGIT_VALUES[currentContext[0]] >= DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]];
-    // } else if (currentContext[1] === 'X' && DIGIT_VALUES[currentContext[0]] < DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]] - (DIGIT_VALUES[currentContext[0]] * 2);
-    // } else if (currentContext[1] === 'L' && DIGIT_VALUES[currentContext[0]] >= DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]];
-    // } else if (currentContext[1] === 'L' && DIGIT_VALUES[currentContext[0]] < DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]] - (DIGIT_VALUES[currentContext[0]] * 2);
-    // } else if (currentContext[1] === 'C' && DIGIT_VALUES[currentContext[0]] >= DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]];
-    // } else if (currentContext[1] === 'C' && DIGIT_VALUES[currentContext[0]] < DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]] - (DIGIT_VALUES[currentContext[0]] * 2);
-    // } else if (currentContext[1] === 'D' && DIGIT_VALUES[currentContext[0]] >= DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]];
-    // } else if (currentContext[1] === 'D' && DIGIT_VALUES[currentContext[0]] < DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]] - (DIGIT_VALUES[currentContext[0]] * 2);
-    // } else if (currentContext[1] === 'M' && DIGIT_VALUES[currentContext[0]] >= DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]];
-    // } else if (currentContext[1] === 'M' && DIGIT_VALUES[currentContext[0]] < DIGIT_VALUES[currentContext[1]]) {
-    //   value += DIGIT_VALUES[currentContext[1]] - (DIGIT_VALUES[currentContext[0]] * 2);
-    // } else {
-    //   console.log('we have an edge case! --- here is context --- ', currentContext);
-    // }
-
     currentContext = next(romanNumeral);
   }
   return value;
