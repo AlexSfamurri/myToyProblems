04390c300e4f9bd2159f4fac4667d12dadb9f1bd
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-26 09:34:44 -0500

    finally made since once i remembered about apply

--- problems/functionBind.js
+++ problems/functionBind.js
@@ -1,10 +1,20 @@
  /*! toyproblems v0.0.0 - MIT license */
 'use strict';
 /* eslint-disable no-extend-native */
-var bind = function (func, context) {
+const bind = function (func, context, ...args) {
   // TODO: Your code here
+  // console.log(context);
+  
+  return function(...stuff){
+    
+    return func.apply(context, args.concat(stuff));
+  };
 };
 
-Function.prototype.bind = function (context) {
+Function.prototype.bind = function (context, ...args) {
   // TODO: Your code here
+  const func = this;
+  return function(...stuff){
+    return func.apply(context, args.concat(stuff));
+  };
 };
--- spec/functionBindSpec.js
+++ spec/functionBindSpec.js
@@ -1,19 +1,57 @@
 /* global toyproblems, describe, it, expect, should */
 
-describe('toyproblems()', function () {
+describe('bind()', function () {
   'use strict';
 
   it('exists', function () {
-    expect(toyproblems).to.be.a('function');
+    expect(bind).to.be.a('function');
 
   });
 
-  it('does something', function () {
-    expect(true).to.equal(false);
+  it('should return a function', function () {
+    var func = function () { };
+    var res = bind(func);
+    should.exist(res);
+    res.should.be.a.Function;
   });
 
-  it('does something else', function () {
-    expect(true).to.equal(false);
+  it('should not return the same function', function () {
+    var func = function () { };
+    var res = bind(func);
+    //aka, the original function should not be returned by `bind()`
+    // instead, we want to return a new function that will call our original
+    // function with the default (aka, curried) arguments we specified in our
+    // original call to `bind`.
+    res.should.not.be.equal(func);
+  });
+
+  it('should return a result when calling the bounded function', function(){
+    var func = function (arg1) { return arg1 };
+    var context = null;
+    var boundFunc = bind(func, context, "foobar");
+    var result = boundFunc();
+    // console.log(result);
+    
+    should.exist(result);
+    result.should.be.equal('foobar');
+  });
+
+  it('should allow the bound function to be called multiple times with different arguments', function(){
+    var func = function (a, b) { return a + b };
+    var context = null;
+    var boundFunc = bind(func, context, 'wow '); // "bind" 'wow ' => the first argument (a)
+    var result1 = boundFunc('dawg'); // first call to boundFunc with b='dawg'
+    should.exist(result1);
+    result1.should.be.equal('wow dawg');
+    var result2 = boundFunc('man'); // second call to boundFunc with b='man'
+    should.exist(result2);
+    result2.should.be.equal('wow man');
+// you're probably modifying some shared `args` array in the
+// clojure scope ( or possibly the global scope :( ) everytime `boundFunc`
+// is being called.
+// hint: myArray.push.call(myArray, [1,2,3]) will _append_ to `myArray`
+// hint: but myArray.concat([1,2,3]) will _return_ a new array and _not_
+// hint: modify `myArray`
   });
 
   // Add more assertions here
