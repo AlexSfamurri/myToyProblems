63aa1e390f9f8f4dc3c9d5ecbe7990eb26e5305e
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-18 09:40:13 -0500

    this solution doesn't work

--- problems/insertSort.js
+++ problems/insertSort.js
@@ -9,8 +9,34 @@ function insertionSort(array) {
   if (typeof tempArray[0] === 'number') {
     tempArray = testingTransform(tempArray);
   }
+  let insert = undefined;
 
+  while (insert !== null) {
+    tempArray = tempArray.map((current, index) => {
+      if (insert === undefined) {
+        insert = current;
+      }
+      if (insert.value < current.value) {
+        let replace = insert;
+        insert = current;
+        return replace;
+      }
+      if (insert.value === current.value && insert.order < current.order) {
+        let replace = insert;
+        insert = current;
+        return replace;
+      }
+      if (index === tempArray.length - 1 && insert === tempArray[index]) {
+        insert = null;
+      }
+      return current;
+    })
+  }
   
+  tempArray.forEach((obj, index) => {
+    array[index] = obj;
+  })
+
 
 
   return array;
